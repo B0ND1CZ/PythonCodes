f8525f435f8a9b55c0e1d1fb79d9d1af04ac789c
B0ND1CZ <101232883+B0ND1CZ@users.noreply.github.com>
2022-05-31 14:23:50 -0300

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,10 @@
+nota_1=float(input("Digite aqui a nota"))
+nota_2= float(input("Digite aqui a nota"))
+nota_3=float(input("Digite aqui a nota"))
+
+peso_1=nota_1*2
+peso_2=nota_2*4
+peso_3=nota_3*6
+
+media=(peso_1+peso_2+peso_3)/12
+print("sua média é", media)
